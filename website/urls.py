0405c3719d5c1b6674570b8e54ee8a7b1e04ab3a
MaximoRdz <mxrdhr@gmail.com>
2023-11-18 12:53:23 +0100

    Login feature

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),       # since located in root only ''
+    # path('/login', views.login_user, name='login'),
+    # path('/logout', views.logout_user, name='logout'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+# django authentication login system
+from django.contrib.auth import authenticate, login, logout
+
+# pop up messages
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    # Check if logging in 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logg In Completed Successfully!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error While Logging In. \nPlease Try Again.")
+            return redirect('home')
+    else:       
+        return render(request, 'home.html', {})
+
+
+def login_user(request):
+    pass
+
+
+def logout_user(request):
+    pass
