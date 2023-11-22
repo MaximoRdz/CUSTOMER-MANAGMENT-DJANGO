6512889d843f104f86253fd5bc3387211a15f823
MaximoRdz <mxrdhr@gmail.com>
2023-11-22 18:58:13 +0100

    Show Records Feature

--- website/views.py
+++ website/views.py
@@ -71,3 +71,15 @@ def register_user(request):
     return render(request, 'register.html', {"form": form})
     
 
+def customer_record(request, pk):
+    # pk: primary key
+    # check if the user is authenticated
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {"customer_record": customer_record})
+    else:
+        messages.success(request, "You Must Be Logged In To Access Records")
+        return redirect('home') 
+
+
