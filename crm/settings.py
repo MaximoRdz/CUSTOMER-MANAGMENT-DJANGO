30aa4a08bac782b26095df7ebbc4301f8562b617
Máximo Rodríguez <mxrdhr@gmail.com>
2023-11-28 18:20:02 +0100

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,8 +78,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "crm_db",
-        "USER": "root", 
-        "PASSWORD": "SQL_Maxi", 
+        "USER": os.environ.get("mysql_user"), 
+        "PASSWORD": os.environ.get("mysql_password"), 
         "HOST": "localhost", 
         "PORT": "3306",
     }
